f7189a9f3dd9e21aa8506566b6838009cc969977
edu <edun327@gmail.com>
2016-10-07 02:01:43 -0300

    Una mejora al kHeapsort, despues evaluar si va o no

--- TP1/src/main/estadisticoDeOrdenK/KHeapsort.java
+++ TP1/src/main/estadisticoDeOrdenK/KHeapsort.java
@@ -1,6 +1,8 @@
 package estadisticoDeOrdenK;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * @author Edu
@@ -15,14 +17,25 @@ public class KHeapsort implements BuscadorEstadisticoDeOrdenK {
 		if (k < 0 || k >= conjunto.length){
 			return null;
 		}
+		Queue<Integer> heap = null;
 		
-		PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+		//Dependiendo el k tengo que ver que heap me convendria, si es menor
+		//a la mitad uso un minHeap, sino un maxHeap
+		boolean kMenorALaMitad = k > conjunto.length/2;
+		
+		if (kMenorALaMitad) {
+			heap = new PriorityQueue<Integer>(conjunto.length,Collections.reverseOrder());
+		} else{
+			heap = new PriorityQueue<Integer>(conjunto.length);
+		}
 		
 		for ( int i = 0; i < conjunto.length; i++){
 			heap.add(conjunto[i]);
 		}
 		
-		for (int i = 0; i < k; i++){
+		int lastIndex = (kMenorALaMitad) ? conjunto.length - 1 - k : k;
+		
+		for (int i = 0; i < lastIndex; i++){
 			heap.remove();
 		}
 		
