a3887a96dda5e029c3040bd67b177169bd08e409
Gaston Snaider <gastonsnaider93@gmail.com>
2016-10-14 16:30:08 -0300

    Agregando test complejo para Dijkstra.

--- TP1/src/test/caminoMinimo/DijkstraTest.java
+++ TP1/src/test/caminoMinimo/DijkstraTest.java
@@ -34,12 +34,12 @@ public class DijkstraTest {
 
 	@Test
 	public void grafoConDosAristasTest() {
-		Digrafo graph = new Digrafo(3);
-		graph.agregarArista(0, 1, 1);
-		graph.agregarArista(0, 2, 5);
-		graph.agregarArista(1, 2, 2);
+		Digrafo grafo = new Digrafo(3);
+		grafo.agregarArista(0, 1, 1);
+		grafo.agregarArista(0, 2, 5);
+		grafo.agregarArista(1, 2, 2);
 		
-		Caminos camino = new Dijkstra(graph, 0, 2);
+		Caminos camino = new Dijkstra(grafo, 0, 2);
 		assertTrue(camino.distancia(0) == 0);
 		assertTrue(camino.distancia(1) == 1);
 		assertTrue(camino.distancia(2) == 3);
@@ -62,11 +62,48 @@ public class DijkstraTest {
 			assertEquals(arista.getDst(), caminoHacia2[i + 1]);
 		}
 	}
-
+	
 	@Test
 	public void grafoComplejoTest() {
-	
+		Digrafo grafo = new Digrafo(7);
 		
+		grafo.agregarArista(0, 1, 3);
+		grafo.agregarArista(0, 2, 5);
+		grafo.agregarArista(0, 3, 6);
+		
+		grafo.agregarArista(1, 3, 2);
+		
+		grafo.agregarArista(2, 3, 2);
+		grafo.agregarArista(2, 4, 6);
+		grafo.agregarArista(2, 5, 3);
+		grafo.agregarArista(2, 6, 7);
+		
+		grafo.agregarArista(3, 5, 9);
+		
+		grafo.agregarArista(4, 6, 2);
+		
+		grafo.agregarArista(5, 4, 5);
+		
+		grafo.agregarArista(5, 6, 1);
+		
+		Caminos camino = new Dijkstra(grafo, 0, 2);
+		assertTrue(camino.distancia(0) == 0);
+		assertTrue(camino.distancia(1) == 3);
+		assertTrue(camino.distancia(2) == 5);
+		assertTrue(camino.distancia(3) == 5);
+		assertTrue(camino.distancia(4) == 11);
+		assertTrue(camino.distancia(5) == 8);
+		assertTrue(camino.distancia(6) == 9);
+		
+		
+		Integer[] caminoHacia6 = {0, 2, 5, 6};
+		List<Arista> aristasHacia6 = camino.camino(6);
+		
+		for (int i = 0; i < aristasHacia6.size(); i++){
+			Arista arista = aristasHacia6.get(i);
+			assertEquals(arista.getSrc(), caminoHacia6[i]);
+			assertEquals(arista.getDst(), caminoHacia6[i + 1]);
+		}
 		
 	}
 }
