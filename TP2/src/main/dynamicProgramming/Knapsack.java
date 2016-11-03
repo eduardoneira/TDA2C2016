ef29e7b6f6cd3af8be4b8b4469ff77ec8edae5dd
edu <edun327@gmail.com>
2016-11-03 01:39:12 -0300

    mochila terminada, falta crear test

--- TP2/src/main/dynamicProgramming/Knapsack.java
+++ TP2/src/main/dynamicProgramming/Knapsack.java
@@ -1,5 +1,6 @@
 package dynamicProgramming;
 
+import java.util.BitSet;
 import java.util.List;
 
 public class Knapsack {
@@ -8,6 +9,8 @@ public class Knapsack {
 	private Integer maxWeight;
 	private KnapsackSolution solution;
 	private Integer id;
+	//Must include maxWeight
+	private Integer[][] dynamicProgrammingMatrix = new Integer[items.size()][maxWeight+1];
 	
 	public Knapsack(List<Item> items, Integer maxWeight, Integer id) {
 		this.items = items;
@@ -16,12 +19,54 @@ public class Knapsack {
 		this.solve();
 	}
 	
-	private void solve(){
-		this.buildSolution();
+	private void solve() { // O(n*w)
+		Long startTimer = System.nanoTime();
+		
+		// init first column
+		for (int i = 0; i < items.size(); i++){
+			dynamicProgrammingMatrix[i][0] = 0;
+		}
+		
+		//init first row
+		for (int w = 1; w <= maxWeight; w++) {
+			if (w < items.get(0).getWeight()) {
+				dynamicProgrammingMatrix[0][w] = 0;
+			} else {
+				dynamicProgrammingMatrix[0][w] = items.get(0).getWeight();
+			}
+		}
+		
+		for (int i = 1; i < items.size(); i++) {
+			Integer currentWeight = items.get(i).getWeight();
+			Integer currentValue = items.get(i).getValue();
+			for (int w = 1; w <= maxWeight; w++) {				
+				if (w < currentWeight){
+					dynamicProgrammingMatrix[i][w] = dynamicProgrammingMatrix[i-1][w];
+				} else {
+					dynamicProgrammingMatrix[i][w] = this.max(currentValue + dynamicProgrammingMatrix[i-1][w-currentWeight], dynamicProgrammingMatrix[i-1][w]);
+				}
+			}
+		}
+		
+		this.buildSolution(System.nanoTime() - startTimer);
 	}
 	
-	private void buildSolution() {
+	private Integer max(Integer a, Integer b) {
+		return (a > b) ? a : b;
+	}
+	
+	private void buildSolution(Long time) {
+		BitSet selected = new BitSet(items.size());
 		
+		Integer w = maxWeight + 1;
+		for (int i = items.size()-1; i >= 0; i--){
+			if (!dynamicProgrammingMatrix[i][w].equals(dynamicProgrammingMatrix[i-1][w])) {
+				selected.set(i);
+				w -= items.get(i).getWeight();
+			}
+		}
+		
+		this.solution = new KnapsackSolution(selected, dynamicProgrammingMatrix[items.size()][maxWeight+1], time);
 	}
 	
 	private KnapsackSolution getSolution() {
--- TP2/src/main/dynamicProgramming/KnapsackSolution.java
+++ TP2/src/main/dynamicProgramming/KnapsackSolution.java
@@ -5,13 +5,21 @@ import java.util.BitSet;
 public class KnapsackSolution {
 	private BitSet selected;
 	private Integer bestValue;
-	private String time; 			//Deberia ser siempre a dos decimales
+	//Tiene que ser siempre a dos decimales
+	private String time; 						
 	
 	public KnapsackSolution(BitSet selected, Integer bestValue, String time) {
 		super();
 		this.selected = selected;
 		this.bestValue = bestValue;
 		this.time = time;
+	}
+	
+	public KnapsackSolution(BitSet selected, Integer bestValue, Long time) {
+		super();
+		this.selected = selected;
+		this.bestValue = bestValue;
+		this.setTime(time);;
 	}
 	
 	public BitSet getSelected() {
@@ -35,5 +43,13 @@ public class KnapsackSolution {
 	public void setTime(Double time){
 		this.time = String.format("%.2f",Math.round(time*100.0)/100.0);
 	}
+	//El tiempo tiene que ser nanosegundos
+	public void setTime(Long time){ 			
+		//TODO: checkear esto
+		double seconds = (double)time / 10000000.0;
+		String aux = String.valueOf(seconds);
+		this.time = aux.substring(0,1) + "." + aux.substring(1);
+	}
+	
 	
 }
