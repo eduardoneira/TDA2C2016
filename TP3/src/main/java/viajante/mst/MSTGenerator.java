02d8ef19b580d4d8a65516976804b803e511d323
Gaston Snaider <gastonsnaider93@gmail.com>
2016-12-05 18:51:17 -0300

    sacando comentarios de cormen

--- TP3/src/main/java/viajante/mst/MSTGenerator.java
+++ TP3/src/main/java/viajante/mst/MSTGenerator.java
@@ -92,20 +92,4 @@ public class MSTGenerator {
 		minHeap.add(node);
 	}
 	
-	
-	/* Cormen
-	
-	MST-Prim(G, w, r):
-		for each u in G.V
-			u.key = infinite
-			u.parent = NIL
-		r.key = 0
-		Q = G.V
-		while !Q.isEmpty:
-			u = Q.poll
-			for each v in G.Adj(u):
-				if v in Q and w(u,v) < v.key
-					v.parent = u
-					v.key = w(u,v)
-	*/
 }
--- TP3/src/main/java/viajante/tsp/TSPHeuristic.java
+++ TP3/src/main/java/viajante/tsp/TSPHeuristic.java
@@ -33,15 +33,6 @@ public class TSPHeuristic {
 		
 		calculateDistance();
 		
-		/* Cormen
-		 
-		1 select a vertex r from G.V to be a "root" vertex.
-		2 compute a minimum spanning tree T for G from root r using MST-PRIM(G, c, r)
-		3 let H be a list of vertices, ordered according to when they are first visited 
-		in a preorder tree walk of T
-		4 return the hamiltonian cycle H
-		*/
-		
 	}
 
 	private void calculateDistance() {
